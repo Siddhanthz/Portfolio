677913ec1c63ee8101d89e635645d05dc1a749cd
Siddhant Jain <siddhanthz29@gmail.com>
2021-08-03 15:00:26 +0530

    init

---
+++ assets/js/lightbox.js
@@ -0,0 +1,8 @@
+
+const lightbox = GLightbox({
+    autoplayVideos: true,
+     'href': 'https://www.youtube.com/watch?v=emCnlByM6rQ',
+     'type': 'video',
+     'source': 'youtube', //vimeo, youtube or local
+     'width': 900,   
+});
